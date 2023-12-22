00be70db195ebb858bf657d75c3fc834ee557f1a
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2023-12-22 22:35:22 +0000

    Initial setup

---
+++ src/FaceDetector.java
@@ -0,0 +1,21 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.dnn.DetectionModel;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
+
+/**
+ * FaceDetector
+ */
+public class FaceDetector {
+
+    public static void main(String[] args) {
+        System.out.println("erro");
+    }
+}
